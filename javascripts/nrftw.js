7d3df62db89066231961ecf71a9d938aaab4d23a
Mark Stickley <markstickley@theyakshack.co.uk>
2013-02-01 00:41:22 +0000

    Site updated at 2013-02-01 00:41:22 UTC

--- javascripts/nrftw.js
+++ javascripts/nrftw.js
@@ -1,42 +1,65 @@
 (function(d){
-	var banner = d.getElementById('banner');
 
-	banner.parentNode.className = getSeason();
+    function setBanner() {
+        var banner = d.getElementById('banner');
+        banner.parentNode.className = getSeason();
+    }
 
-	function getSeason(){
-		var date = new Date(),
-			month = date.getMonth()+1,
-			day = date.getDay();
+    function addRss() {
+        var i, rssElement,
+            articleMetas = d.querySelectorAll('article footer .meta:first-of-type');
 
-		if(
-			(month === 3 && day >= 21)
-			|| month === 4
-			|| month === 5
-			|| (month === 6 && day < 21)
-		){
-			return 'spring';
-		}
+        for(i=0; i<articleMetas.length; i++) {
+            rssElement = document.createElement('p');
+            rssElement.className = 'rssCta';
+            rssElement.innerHTML = '<a href="'+d.rssUrl+'" title="Subscribe to the RSS feed now!">Subscribe to the RSS feed now!</a>';
 
-		else if(
-			(month === 6 && day >= 21)
-			|| month === 7
-			|| month === 8
-			|| (month === 9 && day < 23)
-		){
-			return 'summer';
-		}
+            insertAfter(articleMetas[i], rssElement);
+        }
+    }
 
-		else if(
-			(month === 9 && day >= 23)
-			|| month === 10
-			|| month === 11
-			|| (month === 12 && day < 21)
-		){
-			return 'autumn';
-		}
+    function insertAfter(referenceNode, newNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    }
 
-		else{
-			return 'winter';
-		}
-	}
+    function getSeason(){
+        var date = new Date(),
+            month = date.getMonth()+1,
+            day = date.getDay();
+
+        if(
+            (month === 3 && day >= 21) ||
+            month === 4 ||
+            month === 5 ||
+            (month === 6 && day < 21)
+        ){
+            return 'spring';
+        }
+
+        else if(
+            (month === 6 && day >= 21) ||
+            month === 7 ||
+            month === 8 ||
+            (month === 9 && day < 23)
+        ){
+            return 'summer';
+        }
+
+        else if(
+            (month === 9 && day >= 23) ||
+            month === 10 ||
+            month === 11 ||
+            (month === 12 && day < 21)
+        ){
+            return 'autumn';
+        }
+
+        else{
+            return 'winter';
+        }
+    }
+
+    setBanner();
+    addRss();
+
 })(window.document);
